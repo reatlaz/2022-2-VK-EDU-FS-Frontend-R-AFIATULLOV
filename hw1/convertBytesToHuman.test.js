729ad33201739cb5a649cf5060261ccc4ba43445
playatlas <atlasafri@gmail.com>
2022-10-23 22:23:23 +0300

    setting up github action

---
+++ hw1/convertBytesToHuman.test.js
@@ -0,0 +1,23 @@
+/*
+ * Необходимо покрыть все возможные
+ * и невозможные кейсы. Например,
+ * convertBytesToHuman(-1) === false,
+ * convertBytesToHuman(-1) !== '1 B',
+ * convertBytesToHuman('string') === false
+ * convertBytesToHuman(5) === '5 B'
+ */
+
+import convertBytesToHuman from './convertBytesToHuman';
+
+test('Возвращает false для неправильного типа данных', () => {
+  expect(convertBytesToHuman('abc')).toBe(false)
+  expect(convertBytesToHuman(-1)).toBe(false)
+  expect(convertBytesToHuman(-1234)).toBe(false)
+});
+
+test('Возвращает корректное значение для чисел', () => {
+  expect(convertBytesToHuman(1024)).toBe('1 KB')
+  expect(convertBytesToHuman(123123123)).toBe('117.42 MB')
+  expect(convertBytesToHuman(10)).toBe('10 B')
+});
+
