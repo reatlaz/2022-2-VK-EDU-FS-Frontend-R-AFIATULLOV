a9053ba6790231772a73e4132497e68c19de30fc
playatlas <atlasafri@gmail.com>
2022-10-24 20:37:16 +0300

    fixed localstorage and attempted adding picture to build

--- my-project/simple-chat/webpack.config.js
+++ my-project/simple-chat/webpack.config.js
@@ -54,6 +54,19 @@ module.exports = {
                     },
                 ],
             },
+
+            {
+                test: /static\/barsiq\.(gif|png|jpe?g)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'static/'
+                        }
+                    }
+                ]
+            },
             
         ],
     },
