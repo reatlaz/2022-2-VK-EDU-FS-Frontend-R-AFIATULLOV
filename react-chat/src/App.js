eb1f1351f99f9779f73b11ea81763320860cb01b
playatlas <atlasafri@gmail.com>
2022-12-04 18:29:36 +0300

    chat page ready

--- react-chat/src/App.js
+++ react-chat/src/App.js
@@ -1,12 +1,27 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import {PageChat} from './pages/PageChat';
 
-function App() {
-  return (
-    <PageChat/>
-  );
+class App extends React.Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: PageChat,
+    };
+  }
+  goTo(nextPage) {
+    this.setState({
+      currentPage: nextPage, 
+    })
+  }
+  render() {
+    return (
+      React.createElement(this.state.currentPage,
+        {goToPage: (page) => this.goTo(page)})
+    );
+  }
 }
 
 export default App;
--- react-chat/src/pages/PageChat/PageChat.jsx
+++ react-chat/src/pages/PageChat/PageChat.jsx
@@ -1,32 +1,105 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './PageChat.scss';
-
-
 import {Message, Button} from '../../components';
-import { grey } from '@mui/material/colors';
 
 function Messages(props) {
+  
+  const messages = props.messages;
+  var messagesJSX = null
+  if (messages !== null) {
+    messagesJSX = messages.map((msg, index) =>
+      <Message
+        key={index}
+        isMine={msg.isMine}
+        text={msg.text}
+        time={msg.time}
+      />)
+  }
   return (
     <div id="messages">
-      <Message
-        isMine={false}
-        text={'Я вазу уронил'}
-        time='11:30'
-      />
-      <Message
-        isMine={true}
-        text={'Я вазу уронил'}
-        time='11:30'
-      />
+      {messagesJSX}
     </div>
+  )
+}
+
+function MessageInputForm(props) {
+  const [value, setValue] = useState('');
+
+  const getCurrentTime = () => {
+    const currentDate = new Date();
+      let hours = String(currentDate.getHours());
+      if (hours.length === 1) {
+          hours = '0' + hours
+      }
+      let minutes = String(currentDate.getMinutes());
+
+      if (minutes.length === 1) {
+          minutes = '0' + minutes
+      }
+      return hours + ":" + minutes;
+  }
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    if(value !== '') {
+      const textString = value;
+      const timeString = getCurrentTime()
+      const newMessage = {
+        isMine: true,
+        text: textString,
+        time: timeString,
+      }
+      let localStorageMessages = JSON.parse(localStorage.getItem("messages"));
+      if(localStorageMessages === null) {
+          localStorageMessages = [];
+      }
+      localStorageMessages.push(newMessage)
+      localStorage.setItem('messages', JSON.stringify(localStorageMessages));
+      setValue('');
+      console.log(localStorageMessages);
+      props.changeState()
+    }
+  }
+
+  return(
+    <form className="form" onSubmit={handleSubmit}>
+      <div id="text-input">
+        <input className="form-input"
+          placeholder="Cообщение"
+          onChange={handleChange}
+          value={value}
+        />
+        <Button value='attach_file' type='attach-button'/>
+      </div>
+    </form>
   )
 }
 
 export class PageChat extends React.Component {
   constructor(props) {
     super(props);
+    let localStorageMessages = JSON.parse(localStorage.getItem("messages"));
+    if(localStorageMessages === null) {
+      localStorageMessages = [{
+        isMine: false,
+        text: 'Я вазу уронил',
+        time: '00:01',
+        }];
+      localStorage.setItem('messages', JSON.stringify(localStorageMessages));
+    }
+    this.state ={
+      messages: localStorageMessages
+      }
+  };
+  changeState() {
+    this.setState({
+      messages: JSON.parse(localStorage.getItem("messages"))
+      })
   }
-
   render() {
     return (
       <div className='page-chat'>
@@ -50,14 +123,11 @@ export class PageChat extends React.Component {
           <Button type='nav-button' value='search'/>
           <Button type='nav-button' value='more_vert'/>
         </nav>
-
-        <Messages/>
-        <form className="form" action="/">
-          <div id="text-input">
-            <input className="form-input" name="message-text" placeholder="Cообщение" type="text"/>
-              <Button value='attachment' type='attach-button'/>
-          </div>
-        </form>
+        <Messages messages={this.state.messages}/>
+        <MessageInputForm
+          changeState={() => this.changeState()}
+          onSubmit={this.handleSubmit}
+        />
     </div>
     );
   }
