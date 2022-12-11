9453e9b71f09cfff9ad63fa6e85b3b5b442e9edd
playatlas <atlasafri@gmail.com>
2022-12-11 04:17:38 +0300

    fixed mistakes from previous hw

--- react-chat/src/App.js
+++ react-chat/src/App.js
@@ -10,15 +10,11 @@ function App()  {
     <Router>
       <div className='App'>
         <Routes>
-          <Route path='/im/1' element={<PageChat userId='1'/>}/>
-          <Route path='/im/2' element={<PageChat userId='2'/>}/>
-          <Route path='/im/3' element={<PageChat userId='3'/>}/>
-          <Route path='/user/1' element={<PageProfile userId='1'/>}/>
-          <Route path='/user/2' element={<PageProfile userId='2'/>}/>
-          <Route path='/user/3' element={<PageProfile userId='3'/>}/>
           <Route path='/im' element={<PageChatList/>}/>
+          <Route path='' element={<PageChatList/>}/>
+          <Route path='/im/:id' element={<PageChat />}/>
+          <Route path='/user/:id' element={<PageProfile />}/>
         </Routes>
-
       </div>
     </Router>
   );
--- react-chat/src/pages/PageChat/PageChat.jsx
+++ react-chat/src/pages/PageChat/PageChat.jsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom'
+import React, {useState, useEffect} from 'react';
+import {Link, useParams} from 'react-router-dom'
 import './PageChat.scss';
 import {Message, Button} from '../../components';
 import barsiq from '../../images/barsiq.png';
@@ -66,26 +66,27 @@ function MessageInputForm(props) {
   )
 }
 
-export class PageChat extends React.Component {
-  constructor(props) {
-    super(props);
-    let localStorageMessages = JSON.parse(localStorage.getItem("messages" + this.props.userId));
+export function PageChat () {
+  let { id } = useParams();
+  const [messages, setMessages] = useState([]);
+  useEffect( () => {
+    let localStorageMessages = JSON.parse(localStorage.getItem("messages" + id));
     if(localStorageMessages === null) {
-      if (this.props.userId === '1') {
+      if (id === '1') {
         localStorageMessages = [{
         isMine: false,
         text: 'Я вазу уронил',
         time: '00:01',
         }];
         localStorage.setItem('messages1', JSON.stringify(localStorageMessages));
-      } else if (this.props.userId === '2') {
+      } else if (id === '2') {
         localStorageMessages = [{
         isMine: false,
         text: 'Do you like what you see?',
         time: '00:03',
         }];
         localStorage.setItem('messages2', JSON.stringify(localStorageMessages));
-      } else if (this.props.userId === '3') {
+      } else if (id === '3') {
         localStorageMessages = [{
         isMine: false,
         text: 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF',
@@ -95,59 +96,53 @@ export class PageChat extends React.Component {
       }
       
     }
-    this.state ={
-      messages: localStorageMessages
-      }
-  };
-  changeState(props) {
-    this.setState({
-      messages: JSON.parse(localStorage.getItem("messages" + this.props.userId))
-      })
+    setMessages(localStorageMessages);
+  }, [id]);
+  const changeState = (props) => {
+    setMessages(JSON.parse(localStorage.getItem("messages" + id)))
   }
-  render() {
-    return (
-      <div className='page-chat'>
-        <nav>
-          <Button
-            className='nav-button'
-            value='arrow_back'
-            goTo={'/im'}
-          />
-          <Link className="chat-heading" to={'/user/' + this.props.userId}>
-            <img
-              src={
-                (this.props.userId === '1' && barsiq) ||
-                (this.props.userId === '2' && billy) ||
-                (this.props.userId === '3' && smesh)
-                }
-              className="user-avatar"
-              alt="Not found"
-            />
-            <div className="receiver-text">
-              <div className="username">
-                {
-                  (this.props.userId === '1' && 'Барсик') ||
-                  (this.props.userId === '2' && 'Billy') ||
-                  (this.props.userId === '3' && 'Беседа классааааааааааааааааааааа')
-                }
-              </div>
-              <div className="last-seen">
-                был 2 часа назад
-              </div>
-            </div> 
-          </Link>
-          <Button className='nav-button' value='search'/>
-          <Button className='nav-button' value='more_vert'/>
-        </nav>
-        <Messages messages={this.state.messages}/>
-        <MessageInputForm
-          changeState={() => this.changeState()}
-          onSubmit={this.handleSubmit}
-          userId={this.props.userId}
+
+  return (
+    <div className='page-chat'>
+      <nav>
+        <Button
+          className='nav-button'
+          value='arrow_back'
+          goTo={'/im'}
         />
-    </div>
-    );
-  }
+        <Link className="chat-heading" to={'/user/' + id}>
+          <img
+            src={
+              (id === '1' && barsiq) ||
+              (id === '2' && billy) ||
+              (id === '3' && smesh)
+              }
+            className="user-avatar"
+            alt="Not found"
+          />
+          <div className="receiver-text">
+            <div className="username">
+              {
+                (id === '1' && 'Барсик') ||
+                (id === '2' && 'Billy') ||
+                (id === '3' && 'Беседа классааааааааааааааааааааа')
+              }
+            </div>
+            <div className="last-seen">
+              был 2 часа назад
+            </div>
+          </div> 
+        </Link>
+        <Button className='nav-button' value='search'/>
+        <Button className='nav-button' value='more_vert'/>
+      </nav>
+      <Messages messages={messages}/>
+      <MessageInputForm
+        changeState={changeState}
+        userId={id}
+      />
+  </div>
+  );
 }
 
 
