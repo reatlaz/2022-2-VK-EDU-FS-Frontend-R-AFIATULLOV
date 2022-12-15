abb6e834b304e88c82791ffb55d08ba674fdc038
playatlas <atlasafri@gmail.com>
2022-12-15 04:39:25 +0300

    scrolling pageprofile to top attempt 2

--- react-chat/src/pages/PageProfile/PageProfile.jsx
+++ react-chat/src/pages/PageProfile/PageProfile.jsx
@@ -1,4 +1,4 @@
-import React, {useDebugValue, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom'
 import { Icon } from '@mui/material';
 import './PageProfile.scss';
